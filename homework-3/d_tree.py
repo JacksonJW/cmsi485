f72cf3ca3a1a62415b334657ab7ac205df87f3d3
Jackson Watkins <jacksonwatkins@Jacksons-MacBook-Pro-2.local>
2018-12-24 18:09:25 -0800

    Added in homework-3 files for AI

---
+++ homework-3/d_tree.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.preprocessing import KBinsDiscretizer
+from sklearn.tree import DecisionTreeClassifier
+# from sklearn.model_selection import cross_val_score
+
+
+COLUMNS = ["age", "work class", "education", "education-num", "marital_status", "occ_code", "relationship", "race", "sex", "cap_gain", "cap_loss", "hours_per_week", "native_country", "income"]  # noqa: E501
+
+# reading training and test data
+df_train = pd.read_csv('income-training.csv', names=COLUMNS)
+df_test = pd.read_csv('income-test.csv', names=COLUMNS)
+
+# f illing Nan's with -999999
+df_train.fillna(-99999)
+df_test.fillna(-99999)
+
+le = preprocessing.LabelEncoder()
+# ohe = OneHotEncoder(sparse=False)
+# ordinal = OrdinalEncoder()
+encoder = le
+
+# fine tuning the data in preprocessing:
+for x in COLUMNS:
+    if df_train[x].dtypes == 'object':
+        df_train_2d = df_train[[x]].copy()
+        df_test_2d = df_test[[x]].copy()
+        df_train[x] = encoder.fit_transform(df_train_2d)
+        df_test[x] = encoder.fit_transform(df_test_2d)
+
+# train the data set
+y = df_train.iloc[:, 13]
+x = df_train.iloc[:, 1:13]
+trainingDataTrained = DecisionTreeClassifier(random_state=0)
+trainingDataTrained.fit(x, y)
+
+# test the accuracy of the trained model with the test dataset
+accuracy = trainingDataTrained.score(df_test.iloc[:, 1:13], df_test.iloc[:, 13])  # noqa: E501
+print('the accuracy of the trained model is {:.2f}'.format(accuracy*100))
